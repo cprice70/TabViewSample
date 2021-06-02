91959a5d1277cf6e64a857908947fa3048894493
Chris Price <cprice1@lci1.com>
2021-06-02 05:54:47 -0400

    Add project files.

---
+++ TabViewSample/TabViewSample/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.CommunityToolkit.UI.Views;
+
+namespace TabViewSample
+{
+    public class MainPageViewModel
+    {
+        public IList Tabs { get; } = new ObservableCollection<string> { "Tab1", "Tab2", "Tab3" };
+    }
+}
